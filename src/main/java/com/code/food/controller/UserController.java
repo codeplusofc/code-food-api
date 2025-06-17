fe8bff761201c7ca99ba7270e950e062065cc715
GuiRuchell <ruchellguilherme100@gmail.com>
2025-06-16 22:05:01 -0300

    adicionando comentarios para melhor leitura do codigo

--- src/main/java/com/code/food/controller/UserController.java
+++ src/main/java/com/code/food/controller/UserController.java
@@ -8,15 +8,22 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+// O @RestController serve para dizer que a classe é um controlador de requisição
+// O @RequestMapping serve para dizer qual caminho a requisição irá percorrer
 @RestController
 @RequestMapping("/user")
-
 public class UserController {
-    @Autowired
-    UserRepository userRepository;
 
+    //O @Autowired serve para inicializar uma determinada dependencia
+    @Autowired
+    private UserRepository userRepository;
+
+    //O @PostMapping serve para indicar que a funcionalidade é uma função responsável por inserção
     @PostMapping
     public UserEntity createUser(@RequestBody UserEntity userEntity) {
         return userRepository.save(userEntity);
     }
+
+    //TODO: @BrunoAligathor deve criar uma funcionalidade que retorne todos os usuários
+    //TODO: @Ingrid deve validar se ao criar um usuário, todos os campos estão sendo preenchidos corretamente
 }
--- src/main/java/com/code/food/entity/UserEntity.java
+++ src/main/java/com/code/food/entity/UserEntity.java
@@ -5,8 +5,10 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
+import java.io.Serializable;
+
 @Entity
-public class UserEntity {
+public class UserEntity implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
