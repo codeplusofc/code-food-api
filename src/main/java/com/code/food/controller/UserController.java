c385b38bebe696b5a553160d5891df74d275d09c
Bruno Eidelwein Glaeser Da Silva <brunoegsilva@hotmail.com>
2025-06-16 21:29:01 -0300

    Criando a funcionalidade para criar o usuário

--- src/main/java/com/code/food/controller/UserController.java
+++ src/main/java/com/code/food/controller/UserController.java
@@ -1,5 +1,22 @@
 package com.code.food.controller;
 
-public class UserController {
+import com.code.food.entity.UserEntity;
+import com.code.food.repository.UserRepository;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/user")
+
+public class UserController {
+    @Autowired
+    UserRepository userRepository;
+
+    @PostMapping
+    public UserEntity createUser(@RequestBody UserEntity userEntity) {
+        return userRepository.save(userEntity);
+    }
 }
