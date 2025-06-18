36b2d76c69ed0e2871ae02bd3484a1340db9a5e8
GuiRuchell <ruchellguilherme100@gmail.com>
2025-06-17 21:33:55 -0300

    removendo códigos da camada de item

--- src/main/java/com/code/food/controller/MenuItemController.java
+++ src/main/java/com/code/food/controller/MenuItemController.java
@@ -1,43 +1,14 @@
 package com.code.food.controller;
 
-import com.code.food.entity.MenuItemEntity;
-import com.code.food.service.MenuItemService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-import java.util.Optional;
-
+// O @RestController serve para dizer que a classe é um controlador de requisição
+// O @RequestMapping serve para dizer qual caminho a requisição irá percorrer
 @RestController
 @RequestMapping("/item")
 public class MenuItemController {
 
-    @Autowired
-    private MenuItemService menuItemService;
-
-    @PostMapping
-    public MenuItemEntity createItem(@RequestBody MenuItemEntity menuItemEntity) {
-        return menuItemService.createItem(menuItemEntity);
-    }
-
-    @GetMapping
-    public List<MenuItemEntity> findAllItems() {
-        return menuItemService.findAllItems();
-    }
-
-    @GetMapping("/{id}")
-    public Optional<MenuItemEntity> getItemById(@PathVariable Long id) {
-        return menuItemService.getItemById(id);
-    }
-
-    @DeleteMapping("/{id}")
-    public void deleteItem(@PathVariable Long id) {
-        menuItemService.deleteItem(id);
-    }
-
-    @PutMapping("/{id}")
-    public MenuItemEntity updateItem(@RequestBody MenuItemEntity newItem, @PathVariable Long id) {
-        return menuItemService.updateItem(newItem, id);
-    }
 
 }
